190b9df86556f0fbaa7538118b00a1f615c55f2a
paschalraj <paschalraj@gmail.com>
2014-12-29 14:05:45 +0530

    changing route for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # root 'application#index'
-   root 'application#hello'
+  # root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
